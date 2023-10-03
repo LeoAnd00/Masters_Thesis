21508a357819b80ce1db78747956eff89b1131aa
Leoan-chalmers <leo.andrekson@gmail.com>
2023-10-03 20:52:25 +0200

    Worked on preprocessing more data
    Worked on preprocessing more data

--- code/data_preprocessing/immune_cells/bone_marrow_human/BM_1/functions/data_preprocessing.py
+++ code/data_preprocessing/immune_cells/bone_marrow_human/BM_1/functions/data_preprocessing.py
@@ -1,14 +1,31 @@
-from scipy.io import mmread
 import pandas as pd
 import numpy as np
 import scanpy as sc
 import seaborn as sns
 import matplotlib.pyplot as plt
-import random
 import warnings
 
 
 def read_sc_data(count_data: str, gene_data: str, barcode_data: str):
+    """
+    Read single-cell RNA sequencing data and associated gene and barcode information.
+
+    Parameters:
+    - count_data (str): The path to the count data file in a format compatible with Scanpy.
+    - gene_data (str): The path to the gene information file in tab-separated format.
+    - barcode_data (str): The path to the barcode information file in tab-separated format.
+
+    Returns:
+    - data (AnnData): An AnnData object containing the count data, gene information, and barcode information.
+
+    This function loads single-cell RNA sequencing data, gene information, and barcode information, and organizes them into
+    an AnnData object. The count data is expected to be in a format supported by Scanpy, and it is transposed to ensure
+    genes are represented as rows and cells as columns. Gene information is used to annotate the genes in the data, and
+    barcode information is used to annotate the cells in the data.
+
+    Example usage:
+    data = read_sc_data('count_matrix.mtx.gz', 'genes.tsv.gz', 'barcodes.tsv.gz')
+    """
 
     #Load data
     data = sc.read(count_data, cache=True).transpose()
@@ -31,6 +48,24 @@ def read_sc_data(count_data: str, gene_data: str, barcode_data: str):
     return data
 
 def log1p_normalize(data):
+    """
+    Perform log1p normalization on single-cell RNA sequencing data.
+
+    Parameters:
+    - data (AnnData): An AnnData object containing the count data to be normalized.
+
+    Returns:
+    - data (AnnData): An AnnData object with log1p normalized count data.
+
+    This function performs log1p normalization on count data from single-cell RNA sequencing. It calculates size factors,
+    applies log1p transformation to the counts, and updates the AnnData object with the log1p normalized counts.
+    data.X will be log1p normalized, but also a layer is created called log1p_counts that also contains the normalized counts
+    and a layer called counts is also created containing the unnormalized data.
+
+    Example usage:
+    data = log1p_normalize(data)
+    """
+
     data.layers["counts"] = data.X.copy()
 
     # Calculate size factor
@@ -45,7 +80,33 @@ def log1p_normalize(data):
 
     return data
 
+def scale_data(data, scale_max=10):
+    """
+    Perform feature-level scaling on input data.
+
+    Parameters:
+        data (numpy.ndarray): Input data with features as columns and samples as rows.
+        scale_max (float): Maximum value to which each feature will be scaled.
+
+    Returns:
+        numpy.ndarray: Scaled data with mean-centered and scaled features.
+    """
+    # Calculate the mean and standard deviation for each feature (column)
+    feature_means = np.mean(data, axis=0)
+    feature_stdevs = np.std(data, axis=0)
+
+    # Clip the scaled values to scale_max
+    scaled_data = np.clip((data - feature_means) / feature_stdevs, -scale_max, scale_max)
+
+    return scaled_data
+
 class QC():
+    """
+    Quality Control (QC) class for single-cell RNA sequencing data.
+
+    This class provides methods for performing quality control on single-cell RNA sequencing data, including
+    Median Absolute Deviation (MAD) based outlier detection and filtering based on various QC metrics.
+    """
 
     def __init__(self):
         pass
@@ -55,10 +116,10 @@ class QC():
         Calculate the Median Absolute Deviation (MAD) of a dataset.
 
         Parameters:
-        data (list or numpy.ndarray): The dataset for which MAD is calculated.
+        - data (list or numpy.ndarray): The dataset for which MAD is calculated.
 
         Returns:
-        float: The Median Absolute Deviation (MAD) of the dataset.
+        - float: The Median Absolute Deviation (MAD) of the dataset.
         """
         median = np.median(data)
         
@@ -69,6 +130,26 @@ class QC():
         return mad
 
     def QC_metric_calc(self, data):
+        """
+        Calculate various quality control metrics for single-cell RNA sequencing data.
+
+        Parameters:
+        - data (AnnData): An AnnData object containing the single-cell RNA sequencing data.
+
+        Returns:
+        AnnData: An AnnData object with additional QC metrics added as observations.
+
+        This method calculates the following QC metrics and adds them as observations to the input AnnData object:
+        - 'n_counts': Sum of counts per cell.
+        - 'log_n_counts': Shifted log of 'n_counts'.
+        - 'n_genes': Number of unique genes expressed per cell.
+        - 'log_n_genes': Shifted log of 'n_genes'.
+        - 'pct_counts_in_top_20_genes': Fraction of total counts among the top 20 genes with the highest counts.
+        - 'mt_frac': Fraction of mitochondrial counts.
+
+        Example usage:
+        data = QC().QC_metric_calc(data)
+        """
 
         # Sum of counts per cell
         data.obs['n_counts'] = data.X.sum(1)
@@ -91,28 +172,68 @@ class QC():
         return data
 
     def MAD_based_outlier(self, data, metric: str, threshold: int = 5):
+        """
+        Detect outliers based on the Median Absolute Deviation (MAD) of a specific metric.
+
+        Parameters:
+        - data (AnnData): An AnnData object containing the single-cell RNA sequencing data.
+        - metric (str): The name of the observation metric to use for outlier detection.
+        - threshold (int): The threshold in MAD units for outlier detection.
+
+        Returns:
+        numpy.ndarray: A boolean array indicating outlier cells.
+
+        This method detects outlier cells in the input AnnData object based on the specified metric and threshold.
+        Outliers are identified using the MAD-based approach.
+
+        Example usage:
+        outlier_cells = QC().MAD_based_outlier(data, "log_n_counts", threshold=5)
+        """
+
         data_metric = data.obs[metric]
         # calculate indexes where outliers are detected
         outlier = (data_metric < np.median(data_metric) - threshold * self.median_absolute_deviation(data_metric)) | (
                     np.median(data_metric) + threshold * self.median_absolute_deviation(data_metric) < data_metric)
         return outlier
 
-    def QC_filter_outliers(self, data, threshold: int = 5):
+    def QC_filter_outliers(self, data, threshold: list = [5,5,5,5], expression_limit: int = 20):
+        """
+        Filter outlier cells from the single-cell RNA sequencing data based on QC metrics.
+
+        Parameters:
+        - data (AnnData): An AnnData object containing the single-cell RNA sequencing data.
+        - threshold (list): A list of threshold values for each QC metric in the following order:
+            - log_n_counts threshold
+            - log_n_genes threshold
+            - pct_counts_in_top_20_genes threshold
+            - mt_frac threshold
+        - expression_limit (int): Threshold of how many cell must have counts of a gene in order for it to be preserved.
+
+        Returns:
+        AnnData: An AnnData object with outlier cells removed.
+
+        This method performs QC filtering on the input AnnData object by removing cells that are identified as outliers
+        based on the specified threshold values for each QC metric. Additionally, it filters out genes with fewer than
+        expression_limit unique cells expressing them.
+
+        Example usage:
+        filtered_data = QC().QC_filter_outliers(data, threshold=[5, 5, 5, 5], expression_limit=20)
+        """
 
         # Ignore FutureWarning
         warnings.filterwarnings("ignore", category=FutureWarning)
 
-        data.obs["outlier"] = (self.MAD_based_outlier(data, "log_n_counts", threshold)
-            | self.MAD_based_outlier(data, "log_n_genes", threshold)
-            | self.MAD_based_outlier(data, "pct_counts_in_top_20_genes", threshold)
-            | self.MAD_based_outlier(data, "mt_frac", threshold)
+        data.obs["outlier"] = (self.MAD_based_outlier(data, "log_n_counts", threshold[0])
+            | self.MAD_based_outlier(data, "log_n_genes", threshold[1])
+            | self.MAD_based_outlier(data, "pct_counts_in_top_20_genes", threshold[2])
+            | self.MAD_based_outlier(data, "mt_frac", threshold[3])
         )
 
         # Print how many detected outliers by each QC metric 
-        outlier1 = (self.MAD_based_outlier(data, "log_n_genes", threshold))
-        outlier2 = (self.MAD_based_outlier(data, "log_n_counts", threshold))
-        outlier3 = (self.MAD_based_outlier(data, "pct_counts_in_top_20_genes", threshold))
-        outlier4 = (self.MAD_based_outlier(data, "mt_frac", threshold))
+        outlier1 = (self.MAD_based_outlier(data, "log_n_genes", threshold[1]))
+        outlier2 = (self.MAD_based_outlier(data, "log_n_counts", threshold[0]))
+        outlier3 = (self.MAD_based_outlier(data, "pct_counts_in_top_20_genes", threshold[2]))
+        outlier4 = (self.MAD_based_outlier(data, "mt_frac", threshold[3]))
         print(f"Number of cells before QC filtering: {data.n_obs}")
         print(f"Number of cells removed by log_n_genes filtering: {sum(1 for item in outlier1 if item)}")
         print(f"Number of cells removed by log_n_counts filtering: {sum(1 for item in outlier2 if item)}")
@@ -126,26 +247,38 @@ class QC():
         #Filter genes:
         print('Number of genes before filtering: {:d}'.format(data.n_vars))
 
-        # Min 20 cells - filters out 0 count genes
-        sc.pp.filter_genes(data, min_cells=20)
-        print('Number of genes after filtering so theres min 20 unique cells per gene: {:d}'.format(data.n_vars))
+        # Min "expression_limit" cells - filters out 0 count genes
+        sc.pp.filter_genes(data, min_cells=expression_limit)
+        print(f'Number of genes after filtering so theres min {expression_limit} unique cells per gene: {data.n_vars}')
 
         return data
     
 class EDA():
+    """
+    Exploratory Data Analysis (EDA) class for single-cell RNA sequencing data.
+
+    This class provides methods for visualizing and exploring single-cell RNA sequencing data, including violin plots,
+    scatter plots for QC metrics, and visualization of normalization effects.
+    """
 
     def __init__(self):
         pass
 
     def ViolinJitter(self, data, y_rows: list, title: str = "Violin Plots", subtitle: list = ["Unfiltered", "QC Filtered"]):
         """
-        Create a violin plot and box plot with jitter on top for specified columns.
+        Create violin plots with jitter for specified columns in single-cell RNA sequencing data.
 
         Parameters:
-        data (list): List containing adata.
-        y_rows (str): The column to be plotted on the y-axis.
-        title (str): The title of the plot.
-        subtitle (list): List of titles for each adata in data for the plot.
+        - data (list of AnnData): A list of AnnData objects, typically representing unfiltered and QC-filtered data.
+        - y_rows (list): A list of column names to plot on the y-axis.
+        - title (str): The title for the entire plot.
+        - subtitle (list): A list of subtitles corresponding to each dataset in 'data'.
+
+        This method generates violin plots with jitter for the specified columns in the single-cell RNA sequencing data.
+        Each dataset in 'data' will be plotted side by side with the specified columns on the y-axis.
+
+        Example usage:
+        EDA().ViolinJitter(data=[data_unfiltered, data_QC_filtered], y_rows=["n_counts", "n_genes"], title="QC Metrics")
         """
 
         # Ignore the specific FutureWarning
@@ -187,12 +320,18 @@ class EDA():
 
     def ScatterForQCMetrics(self, data, title:str = "Scatter Plot", subtitle:list = ["Unfiltered", "QC Filtered"]):
         """
-        Create a scatter plot with specified x and y columns and color the points based on a color column.
+        Create scatter plots for QC metrics in single-cell RNA sequencing data.
 
         Parameters:
-        data (list): List containing adata.
-        title (str): The title of the scatter plot (optional).
-        subtitle (list): List of titles for each adata in data for the plot.
+        - data (list of AnnData): A list of AnnData objects, typically representing unfiltered and QC-filtered data.
+        - title (str): The title for the entire plot.
+        - subtitle (list): A list of subtitles corresponding to each dataset in 'data'.
+
+        This method generates scatter plots for specified QC metrics in the single-cell RNA sequencing data.
+        Each dataset in 'data' will be plotted side by side with the specified QC metrics.
+
+        Example usage:
+        EDA().ScatterForQCMetrics(data=[data_unfiltered, data_QC_filtered], title="QC Metrics Scatter Plot")
         """
 
         # Ignore the specific FutureWarning
@@ -230,6 +369,19 @@ class EDA():
         plt.show()
 
     def VisualizeNormalization(self,data):
+        """
+        Visualize the effects of normalization on single-cell RNA sequencing data.
+
+        Parameters:
+        - data (AnnData): An AnnData object containing the single-cell RNA sequencing data.
+
+        This method generates histograms to visualize the effects of normalization on the single-cell RNA sequencing data.
+        It compares the distribution of raw counts and log1p normalized counts.
+
+        Example usage:
+        EDA().VisualizeNormalization(data)
+        """
+        
         fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
         p1 = sns.histplot(data.obs["n_counts"], bins=100, kde=False, ax=axes[0])
