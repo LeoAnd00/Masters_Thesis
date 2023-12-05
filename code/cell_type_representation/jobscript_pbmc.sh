b7b327676e80a2e5274b34fce02426c3b14c863f
Leo Andrekson <leoan@alvis1.int.private>
2023-12-05 14:40:00 +0100

    Smaller changes

--- code/cell_type_representation/jobscript_pbmc.sh
+++ code/cell_type_representation/jobscript_pbmc.sh
@@ -3,4 +3,4 @@
 #SBATCH -t 08:00:00
 #SBATCH --gpus-per-node=V100:1
 
-apptainer exec /cephyr/users/leoan/Alvis/Masters_Thesis/MScEnv.sif python run_benchmark.py '/cephyr/users/leoan/Alvis/Masters_Thesis/data/processed/immune_cells/merged/PBMC_merged_all.h5ad' 'trained_models/PBMC/' 'benchmarks/results/PBMC/benchmark' '/cephyr/users/leoan/Alvis/Masters_Thesis/data/processed/pathway_information/all_pathways.json' '/cephyr/users/leoan/Alvis/Masters_Thesis/data/raw/gene2vec_embeddings/gene2vec_dim_200_iter_9_w2v.txt' ''
+apptainer exec /cephyr/users/leoan/Alvis/Masters_Thesis/MScEnv.sif python run_benchmark.py '/cephyr/users/leoan/Alvis/Masters_Thesis/data/processed/immune_cells/merged/PBMC_merged_all.h5ad' 'trained_models/PBMC/' 'benchmarks/results/PBMC/benchmark' '/cephyr/users/leoan/Alvis/Masters_Thesis/data/processed/pathway_information/all_pathways.json' '/cephyr/users/leoan/Alvis/Masters_Thesis/data/raw/gene2vec_embeddings/gene2vec_dim_200_iter_9_w2v.txt' '_PBMC/'
