3907db150aaaed1020d36f843364f5bf524094ec
Leoan-chalmers <leo.andrekson@gmail.com>
2024-03-06 10:41:53 +0100

    Updates to visualization
    Updates to visualization

--- code/cell_type_annotation/visualize_metrics.py
+++ code/cell_type_annotation/visualize_metrics.py
@@ -139,8 +139,13 @@ class VisualizeEnv():
 
         metrics = self.metrics.copy()
 
-        metrics['Method'][metrics['Method'] == "Model1"] = "Model1_HVGs"
-        metrics['Method'][metrics['Method'] == "TOSICA"] = "TOSICA_HVGs"
+        metrics['Method'][metrics['Method'] == "Model1"] = "Model1 | HVGs"
+        metrics['Method'][metrics['Method'] == "TOSICA"] = "TOSICA | HVGs"
+        metrics['Method'][metrics['Method'] == "scNym_HVGs"] = "scNym | HVGs"
+        metrics['Method'][metrics['Method'] == "Seurat_HVGs"] = "Seurat | HVGs"
+        metrics['Method'][metrics['Method'] == "SciBet_HVGs"] = "SciBet | HVGs"
+        metrics['Method'][metrics['Method'] == "CellID_cell_HVGs"] = "CellID_cell | HVGs"
+        metrics['Method'][metrics['Method'] == "CellID_group_HVGs"] = "CellID_group | HVGs"
 
         #metrics = metrics.loc[metrics["Dataset"] == dataset_name,:]
 
@@ -162,18 +167,18 @@ class VisualizeEnv():
             variable = visual_metrics[metric].to_list()
             group = visual_metrics['Dataset'].to_list()
             group2 = visual_metrics['Method'].to_list()
-            hue_order = ["Model1_HVGs", 
+            hue_order = ["Model1 | HVGs", 
                          "scNym", 
-                         "scNym_HVGs", 
+                         "scNym | HVGs", 
                          "Seurat", 
-                         "Seurat_HVGs", 
-                         "TOSICA_HVGs", 
+                         "Seurat | HVGs", 
+                         "TOSICA | HVGs", 
                          "SciBet", 
-                         "SciBet_HVGs", 
+                         "SciBet | HVGs", 
                          "CellID_cell", 
                          "CellID_group", 
-                         "CellID_cell_HVGs", 
-                         "CellID_group_HVGs"]
+                         "CellID_cell | HVGs", 
+                         "CellID_group | HVGs"]
             
             #sns.move_legend(axs[col_idx], "upper left", bbox_to_anchor=(1, 0.75))
             sns.boxplot(y = variable,
@@ -232,4 +237,113 @@ class VisualizeEnv():
         if image_path:
             plt.savefig(f'{image_path}.svg', format='svg')
 
+        plt.show()
+
+    def BarPlotAverageMetricsVisualization(self, image_path: str=None):
+        """
+        Generate a bar plot visualization for each metric, displaying the mean values
+        with error bars representing standard deviation across different model types.
+
+        Parameters
+        --------
+        image_path : str, optional 
+            If provided, the plot will be saved as an SVG file with the specified file path/name (.svg is added by the function at the end). Defaults to None (meaning no image will be downloaded).
+        version : int, optional
+            Which plot option to chose (Options: 1, 2, 3)
+            
+        Returns
+        -------
+        None
+        """
+
+        metrics = self.metrics.copy()
+
+        metrics['Method'][metrics['Method'] == "Model1"] = "Model1 | HVGs"
+        metrics['Method'][metrics['Method'] == "TOSICA"] = "TOSICA | HVGs"
+        metrics['Method'][metrics['Method'] == "scNym_HVGs"] = "scNym | HVGs"
+        metrics['Method'][metrics['Method'] == "Seurat_HVGs"] = "Seurat | HVGs"
+        metrics['Method'][metrics['Method'] == "SciBet_HVGs"] = "SciBet | HVGs"
+        metrics['Method'][metrics['Method'] == "CellID_cell_HVGs"] = "CellID_cell | HVGs"
+        metrics['Method'][metrics['Method'] == "CellID_group_HVGs"] = "CellID_group | HVGs"
+
+        # Calculate averge score of each metric in terms of each method and dataset
+        averages = metrics.groupby(["Method", "Dataset"])[["Accuracy", "Balanced Accuracy", "F1 Score"]].mean()
+
+        # Group by "Dataset"
+        grouped_averages = averages.groupby("Dataset")
+
+        # Define min-max normalization function
+        def min_max_normalize(x):
+            return (x - x.min()) / (x.max() - x.min())
+
+        # Apply min-max normalization to each group
+        normalized_averages = grouped_averages.transform(min_max_normalize)
+
+        # Group by "Method"
+        method_averages = normalized_averages.groupby("Method")[["Accuracy", "Balanced Accuracy", "F1 Score"]].mean()
+
+        # Calculate the mean across metrics for each method
+        method_averages['Overall'] = method_averages.mean(axis=1)
+
+        metrics = pd.DataFrame({"Accuracy": method_averages["Accuracy"],
+                                "Balanced Accuracy": method_averages["Balanced Accuracy"],
+                                "F1 Score": method_averages["F1 Score"],
+                                "Overall": method_averages["Overall"],
+                                "Method": method_averages.index})
+        metrics.reset_index(drop=True, inplace=True)
+
+        # Define the order of metrics
+        metric_order = ["Accuracy", "Balanced Accuracy", "F1 Score", "Overall"]
+
+        # Melt the DataFrame to convert it to long format
+        melted_metrics = pd.melt(metrics, id_vars='Method', var_name='Metric', value_name='Value')
+        
+        # Set up the figure and axis
+        plt.figure(figsize=(8, 6))
+
+        # Sort the melted DataFrame by the 'Overall' metric
+        overall_sorted = melted_metrics[melted_metrics['Metric'] == 'Overall'].sort_values(by='Value', ascending=False)
+        melted_metrics = pd.concat([overall_sorted, melted_metrics[melted_metrics['Metric'] != 'Overall']])
+
+        hue_order = ["Model1 | HVGs", 
+                         "scNym", 
+                         "scNym | HVGs", 
+                         "Seurat", 
+                         "Seurat | HVGs", 
+                         "TOSICA | HVGs", 
+                         "SciBet", 
+                         "SciBet | HVGs", 
+                         "CellID_cell", 
+                         "CellID_group", 
+                         "CellID_cell | HVGs", 
+                         "CellID_group | HVGs"]
+
+        # Plot the grouped bar plot with opaque bars and borders
+        sns.barplot(x='Metric', y='Value', hue='Method', data=melted_metrics, hue_order=hue_order, order=metric_order, ci=None, dodge=True, alpha=1.0, edgecolor='black')
+
+        # Get the current ticks and positions for the x-axis
+        x_positions = np.arange(len(metric_order)) 
+
+        # Calculate the positions for the minor grid lines
+        minor_positions = []
+        for i in range(len(metric_order) - 1):
+            minor_positions.append((x_positions[i] + x_positions[i + 1]) / 2)
+
+        # Add minor grid lines between the x positions
+        plt.grid(axis='x', linestyle='--', alpha=1.0, zorder=1, which='minor')
+
+        # Set the positions for the minor ticks
+        plt.gca().set_xticks(minor_positions, minor=True)
+
+        # Adjust legend placement and content orientation
+        plt.legend(title=None, loc='upper left', bbox_to_anchor=(1, 0.7), ncol=1, frameon=False, fontsize='small')
+
+
+        # Set the title and labels
+        plt.ylabel('Score')
+        plt.xlabel('Metric')
+
+        # Show the plot
+        plt.xticks(ha='center')
+        plt.tight_layout()
         plt.show()
